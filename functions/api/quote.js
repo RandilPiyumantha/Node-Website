28cf5a3272cb6284c9504f18736420604864da19
unknown <randilpiyumantha666@gmail.com>
2026-08-19 23:10:17 +0530

    Testing quote.js

--- functions/api/quote.js
+++ functions/api/quote.js
@@ -1,3 +1,23 @@
-export async function onRequest() {
-  return new Response("Node Global quote function is working!");
+export async function onRequestPost(context) {
+  try {
+    const formData = await context.request.formData();
+
+    const data = Object.fromEntries(formData.entries());
+
+    return Response.json({
+      success: true,
+      message: "Quote request received.",
+      data: data,
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unable to process quote request.",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
 }
